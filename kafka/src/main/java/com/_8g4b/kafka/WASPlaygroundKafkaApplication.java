7138921aaecf660098fb0fbbd1dafc126c15b296
snowykte0426 <snowykte0426@naver.com>
2025-11-25 00:06:18 +0900

    Change main method visibility to static in application classes

--- kafka/src/main/java/com/_8g4b/kafka/WASPlaygroundKafkaApplication.java
+++ kafka/src/main/java/com/_8g4b/kafka/WASPlaygroundKafkaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WASPlaygroundKafkaApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(WASPlaygroundKafkaApplication.class, args);
     }
 }
